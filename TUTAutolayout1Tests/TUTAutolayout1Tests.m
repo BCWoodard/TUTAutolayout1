3c003ecc9d50c2dd8e937d7c1f6344d1adaab11b
Brad Woodard <bcwoodard@gmail.com>
2013-12-16 17:49:22 -0600

    Initial commit with .gitignore

---
+++ TUTAutolayout1Tests/TUTAutolayout1Tests.m
@@ -0,0 +1,34 @@
+//
+//  TUTAutolayout1Tests.m
+//  TUTAutolayout1Tests
+//
+//  Created by Brad Woodard on 12/16/13.
+//  Copyright (c) 2013 Brad Woodard. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TUTAutolayout1Tests : XCTestCase
+
+@end
+
+@implementation TUTAutolayout1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
